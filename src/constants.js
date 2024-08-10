53166140e7696c0dc565c25f0e2dd32b4eadee00
Tan Yin Yu <143823075+totoro-7@users.noreply.github.com>
2024-08-11 04:51:18 +0800

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript these days.
+    I've been trying to upskill lately. Anyway regardless of the language, I enjoy programming.
+    Here is my <a href="https://yinyu.netlify.app/" target="_blank">website</a>!`,
+  "cs-degree": `This is my Information Engineering & Media transcript. I hung it on the wall because I'm proud of it & reminds me of what I am working towards!`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I learnt programming concepts and followed projects from tech youtubers, 
+  just like this portfolio you're currently playing through!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. It is the first and last thing I see everyday.`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/uc?export=download&id=1JfbJcOlPkgnFUwJX7id3W3jZISsy02zc" target="_blank">Check it out?</a>
+  You can also contact me at yinyu18.tan@gmail.com.`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS.`,
+  library: `There are a lot of fictional books on my shelves. I love to be transported in these worlds that can only exist within the pages.
+  Though, I'm currently moving towards motivational books.`,
+  exit: `If you want to exit this portfolio, just close the tab.`,
+};
